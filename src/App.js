fc3461c084f9fedeafbd52ba8165a57cac494a9a
cnyt-y <56654010+cnyt-y@users.noreply.github.com>
2023-08-06 17:25:48 +0300

    yeni dosya

--- src/App.js
+++ src/App.js
@@ -1,25 +1,8 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Layout from './sayfalar/Layout'
-import Home from './sayfalar/Home'
-import Blogs from './sayfalar/Blogs'
-import Contact from './sayfalar/Contact'
-import NoPage from './sayfalar/NoPage'
 
 function App() {
   
   return (
-    <BrowserRouter>
-      <Routes>
-
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="blogs" element={<Blogs />} />
-          <Route path="contact" element={<Contact />} />
-          <Route path="*" element={<NoPage />} />
-        </Route>
-        
-      </Routes>
-    </BrowserRouter>
+    merhaba
   );
 }
 
--- src/sayfalar/Blogs.jsx
+++
@@ -1,8 +0,0 @@
-function Blogs() {
-
-    return (
-        <p>Blog yazıları</p>
-    )
-}
-
-export default Blogs
